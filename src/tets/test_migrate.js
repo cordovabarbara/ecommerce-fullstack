303fce5502513132f03c642969438374d544349f
cordovabarbara <aliendocordova@gmail.com>
2023-06-26 22:15:18 -0500

    created token secret and first test

---
+++ src/tets/test_migrate.js
@@ -0,0 +1,11 @@
+const sequelize = require('../utils/connection');
+
+const main = async() => {
+    try{
+        await sequelize.sync({ force: true });
+        console.log('Me ejecute🦄')
+        process.exit();
+    } catch(error){
+        console.log(error);
+    }
+ }
---
+++ src/tets/user.js
@@ -0,0 +1,14 @@
+const User = require("../models/User")
+
+const user = async() =>{
+    
+    const userCreate = {
+        firstName:"Anais",
+        lastName:"Aliendo",
+        email:"brbacordova@gmail.com",
+        password:"123456",
+        phone:"1234567"
+    }
+
+    await User.create
+}
